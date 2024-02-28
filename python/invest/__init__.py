f2b87b26ef00f77178183fc77bcda6858888b390
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2024-02-28 13:33:48 +0900

    update

--- python/invest/__init__.py
+++ python/invest/__init__.py
@@ -15,7 +15,23 @@ class Invest:
             _start = "2010-01-01",
             _end = datetime.now()
     ):
+        # Date가 컬럼에 포함되어있다면 인덱스로 변경 
+        if 'Date' in _df.columns:
+            _df.set_index('Date', inplace=True)
+        # 인덱스를 시계열 데이터로 변경 
+        _df.index = pd.to_datetime(_df.index, format='%Y-%m-%d')
         # _df의 결측치와 무한대 제거하고 기준이 되는 컬럼만 두고 나머지 삭제
         flag = _df.isin([np.nan, np.inf, -np.inf]).any(axis=1)
-        self.df = _df.loc[flag, [_col]]
+        self.df = _df.loc[~flag, [_col]]
+        # _start, _end는 시계열 데이터로 변경해서 객체 변수에 대입 
+        try:
+            self.start = datetime.strptime(_start, "%Y-%m-%d")
+            if type(_end) == 'str':
+                self.end = datetime.strptime(_end, '%Y-%m-%d')
+            else:
+                self.end = _end
+        except:
+            print('투자기간 인자값의 포멧이 잘못되었습니다. (YYYY-mm-dd)')
+        self.col = _col
+
         
